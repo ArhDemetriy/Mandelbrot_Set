6340968b4b905ab403c7b2b9782bad51a7cd36a9
Dmitriy Nazimov <ArhDemetriy@GMail.com>
2026-07-23 19:11:52 +0800

    init

---
+++ oxlint.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from "oxlint";
+
+export default defineConfig({
+  settings: {
+    next: { rootDir: "./app" },
+  },
+  plugins: ["react", "typescript", "promise"],
+  categories: {
+    correctness: "warn",
+  },
+  rules: {
+    "eslint/no-unused-vars": "error",
+  },
+});
