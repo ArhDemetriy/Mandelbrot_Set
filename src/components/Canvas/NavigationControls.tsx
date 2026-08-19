47c90b9a7106e9bcb7f81759bc9850a07e0233e0
Dmitriy Nazimov <ArhDemetriy@GMail.com>
2026-08-19 12:52:48 +0800

    fix move after stop render

--- src/components/Canvas/NavigationControls.tsx
+++ src/components/Canvas/NavigationControls.tsx
@@ -1,14 +1,29 @@
 import { useFrame, useThree } from '@react-three/fiber';
 import { useGesture } from '@use-gesture/react';
 import { type ExtractAtomValue, atom, useAtom, useAtomValue, useSetAtom } from 'jotai';
-import { useEffect, useRef } from 'react';
+import { useAtomCallback } from 'jotai/utils';
+import { useCallback, useEffect } from 'react';
 
 import { moveSpeedAtom, offsetAtom, zoomAtom } from '@/store/fractalStore';
+
+const KEYS_WHITE_LIST = new Set([
+  'KeyA',
+  'ArrowLeft',
+  'KeyD',
+  'ArrowRight',
+  'KeyW',
+  'ArrowUp',
+  'KeyS',
+  'ArrowDown',
+  'KeyE',
+  'KeyQ',
+]);
 
 const keysPressedAtom = atom({} as Record<string, { keydownTimestamp: number } | undefined>);
 const addKeyPressedAtom = atom(
   null,
   (get, set, key: string, payload: ExtractAtomValue<typeof keysPressedAtom>[string]) => {
+    if (!KEYS_WHITE_LIST.has(key)) return;
     const currentState = get(keysPressedAtom);
     set(keysPressedAtom, {
       ...currentState,
@@ -22,16 +37,65 @@ const delKeyPressedAtom = atom(null, (get, set, key: string) => {
     set(keysPressedAtom, Object.fromEntries(Object.entries(currentState).filter(({ 0: k }) => k !== key)));
 });
 
+const deltasAtom = atom(get => {
+  const keys = get(keysPressedAtom);
+  let lastActionTimestamp = Infinity;
+
+  let dxDirection = 0;
+  if (keys['KeyA'] || keys['ArrowLeft']) dxDirection -= 1;
+  if (keys['KeyD'] || keys['ArrowRight']) dxDirection += 1;
+  if (dxDirection !== 0) {
+    lastActionTimestamp = Math.min(
+      lastActionTimestamp,
+      keys['KeyA']?.keydownTimestamp ?? Infinity,
+      keys['ArrowLeft']?.keydownTimestamp ?? Infinity,
+      keys['KeyD']?.keydownTimestamp ?? Infinity,
+      keys['ArrowRight']?.keydownTimestamp ?? Infinity
+    );
+  }
+
+  let dyDirection = 0;
+  if (keys['KeyW'] || keys['ArrowUp']) dyDirection += 1;
+  if (keys['KeyS'] || keys['ArrowDown']) dyDirection -= 1;
+  if (dyDirection !== 0) {
+    lastActionTimestamp = Math.min(
+      lastActionTimestamp,
+      keys['KeyW']?.keydownTimestamp ?? Infinity,
+      keys['ArrowUp']?.keydownTimestamp ?? Infinity,
+      keys['KeyS']?.keydownTimestamp ?? Infinity,
+      keys['ArrowDown']?.keydownTimestamp ?? Infinity
+    );
+  }
+
+  let zoomDirection = 0;
+  if (keys['KeyE']) zoomDirection += 1;
+  if (keys['KeyQ']) zoomDirection -= 1;
+  if (zoomDirection !== 0) {
+    lastActionTimestamp = Math.min(
+      lastActionTimestamp,
+      keys['KeyE']?.keydownTimestamp ?? Infinity,
+      keys['KeyQ']?.keydownTimestamp ?? Infinity
+    );
+  }
+
+  return {
+    dxDirection: dxDirection as 0 | -1 | 1,
+    dyDirection: dyDirection as 0 | -1 | 1,
+    zoomDirection: zoomDirection as 0 | -1 | 1,
+    lastActionTimestamp,
+  };
+});
+
 export function NavigationControls() {
+  const { gl, invalidate } = useThree();
+
   const setOffset = useSetAtom(offsetAtom);
   const [zoom, setZoom] = useAtom(zoomAtom);
   const moveSpeed = useAtomValue(moveSpeedAtom);
 
-  const { gl, invalidate } = useThree();
-
-  const keysPressed = useAtomValue(keysPressedAtom);
   const addKeyPressed = useSetAtom(addKeyPressedAtom);
   const delKeyPressed = useSetAtom(delKeyPressedAtom);
+  const getDeltas = useAtomCallback(useCallback(get => get(deltasAtom), []));
 
   useEffect(() => {
     const abortController = new AbortController();
@@ -81,52 +145,23 @@ export function NavigationControls() {
     }
   );
 
-  const keysPressedRef = useRef(keysPressed);
-  keysPressedRef.current = keysPressed;
   useFrame((_, delta) => {
     const now = performance.now();
-    const keys = keysPressedRef.current;
 
-    const deltaUp = Math.max(
-      0,
-      Math.min(keys['KeyW']?.keydownTimestamp ?? 0, keys['ArrowUp']?.keydownTimestamp ?? 0) - now
-    );
-    const deltaDown = Math.max(
-      0,
-      Math.min(keys['KeyS']?.keydownTimestamp ?? 0, keys['ArrowDown']?.keydownTimestamp ?? 0) - now
-    );
-    const existDeltaY = Boolean(deltaUp) != Boolean(deltaDown);
+    const { dxDirection, dyDirection, zoomDirection, lastActionTimestamp } = getDeltas();
+    const isNoShift = dxDirection === 0 && dyDirection === 0;
+    if (isNoShift && zoomDirection == 0) return;
 
-    const deltaLeft = Math.max(
-      0,
-      Math.min(keys['KeyA']?.keydownTimestamp ?? 0, keys['ArrowLeft']?.keydownTimestamp ?? 0) - now
-    );
-    const deltaRight = Math.max(
-      0,
-      Math.min(keys['KeyD']?.keydownTimestamp ?? 0, keys['ArrowRight']?.keydownTimestamp ?? 0) - now
-    );
-    const existDeltaX = Boolean(deltaLeft) != Boolean(deltaRight);
-
-    let dxDirection = 0;
-    let dyDirection = 0;
-
-    if (keys['KeyW'] || keys['ArrowUp']) dyDirection += 1;
-    if (keys['KeyS'] || keys['ArrowDown']) dyDirection -= 1;
-    if (keys['KeyA'] || keys['ArrowLeft']) dxDirection -= 1;
-    if (keys['KeyD'] || keys['ArrowRight']) dxDirection += 1;
+    const safetyDelta = Math.min(delta, Math.max(0, now - lastActionTimestamp) / 1000);
+    if (safetyDelta <= 0) return;
 
     if (dxDirection === 0 && dyDirection === 0) {
-      // зум считаем только если нет offset
-      let zoomDelta = 0;
-      if (keys['KeyE']) zoomDelta += 1;
-      if (keys['KeyQ']) zoomDelta -= 1;
-      if (zoomDelta !== 0) {
-        const zoomSpeed = 1.5;
-        setZoom(prev => Math.max(0.1, prev * Math.exp(zoomDelta * zoomSpeed * delta)));
-      }
+      const zoomSpeed = 1.5;
+      setZoom(prev => Math.max(0.1, prev * Math.exp(zoomSpeed * zoomDirection * safetyDelta)));
+      return;
     }
 
-    const step = (moveSpeed * delta) / zoom;
+    const step = (moveSpeed * safetyDelta) / zoom;
     setOffset(([x, y]) => [x + dxDirection * step, y + dyDirection * step]);
   });
 
