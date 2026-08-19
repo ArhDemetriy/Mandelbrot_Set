7bfd86e3290ebe60a198e82aa0a5c7af34c5eafa
Dmitriy Nazimov <ArhDemetriy@GMail.com>
2026-08-19 16:31:35 +0800

    init

--- src/components/ui/ControlPanel.tsx
+++ src/components/ui/ControlPanel.tsx
@@ -5,7 +5,6 @@ import { Button } from '@/components/shadcn_ui/button';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/shadcn_ui/card';
 import { Label } from '@/components/shadcn_ui/label';
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/shadcn_ui/select';
-// import { Slider } from '@/components/shadcn_ui/slider';
 import { type ColorPalette, offsetAtom, paletteAtom, resetViewAtom, zoomAtom } from '@/store/fractalStore';
 
 export function ControlPanel() {
