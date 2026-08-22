333b4de61a42ad3c0137c0aad9a7670a9aec0299
ArhDemetriy <arhdemetriy@gmail.com>
2026-08-22 14:49:12 +0800

    init

---
+++ src/engine/passes/ComputePass64.ts
@@ -0,0 +1,118 @@
+import {
+  type DataTexture,
+  GLSL3,
+  ShaderMaterial,
+  type ShaderMaterialProperties,
+  Vector2,
+  Vector3,
+  type WebGLRenderer,
+} from 'three';
+
+import f32Shader from '@/shaders/mandelbrot/2D/mandelbrotF64.frag?raw';
+import vertexShader from '@/shaders/mandelbrot/mandelbrot.vert?raw';
+
+import type { ScissorBox } from '../utils';
+
+export class ComputePass {
+  private readonly material: ShaderMaterial;
+  private readonly quadRender: (props: {
+    gl: WebGLRenderer;
+    material?: ShaderMaterial;
+    scissors?: ScissorBox[];
+  }) => void;
+  constructor({
+    render,
+    ...initUniforms
+  }: {
+    render(props: { gl: WebGLRenderer; material?: ShaderMaterial; scissors?: ScissorBox[] }): void;
+
+    zoom: number;
+    width: number;
+    height: number;
+    iterationsPerFrame: number;
+    maxIterations: number;
+    offset: [number, number];
+    result: DataTexture;
+    state: DataTexture;
+  }) {
+    this.quadRender = render;
+
+    this.material = new ShaderMaterial({
+      glslVersion: GLSL3,
+      fragmentShader: f32Shader,
+      vertexShader,
+      uniforms: ComputePass.makeInitComputeUniforms(initUniforms),
+    });
+  }
+
+  public render({
+    gl,
+    result,
+    state,
+    scissors,
+  }: {
+    gl: WebGLRenderer;
+    result: DataTexture;
+    state: DataTexture;
+    scissors?: ScissorBox[];
+  }) {
+    const uniforms = this.getUniforms();
+    uniforms.u_prev_result.value = result;
+    uniforms.u_prev_state.value = state;
+
+    this.quadRender({ gl, material: this.material, scissors });
+  }
+
+  public setOffset(offset: [number, number]) {
+    this.getUniforms().u_offset.value.set(offset[0], offset[1]);
+  }
+
+  public setScale({ zoom, width, height }: { zoom: number; height: number; width: number }) {
+    this.getUniforms().u_view.value.set(1 / (zoom * height), width / 2, height / 2);
+  }
+
+  public dispose() {
+    this.material.dispose();
+  }
+
+  private getUniforms() {
+    return this.material.uniforms as ReturnType<(typeof ComputePass)['makeInitComputeUniforms']>;
+  }
+
+  private static makeInitComputeUniforms({
+    zoom,
+    width,
+    height,
+    iterationsPerFrame,
+    maxIterations,
+    offset,
+    result,
+    state,
+  }: {
+    zoom: number;
+    height: number;
+    width: number;
+    iterationsPerFrame: number;
+    maxIterations: number;
+    offset: [number, number];
+    result: DataTexture;
+    state: DataTexture;
+  }) {
+    return {
+      u_const: {
+        /** iterationsPerFrame, maxIterations */
+        value: new Vector2(iterationsPerFrame, maxIterations),
+      },
+      u_offset: {
+        /** ...offset */
+        value: new Vector2(offset[0], offset[1]),
+      },
+      u_view: {
+        /** 1/(zoom*height), width/2, height/2  */
+        value: new Vector3(1 / (zoom / height), width / 2, height / 2),
+      },
+      u_prev_result: { value: result },
+      u_prev_state: { value: state },
+    } satisfies ShaderMaterialProperties['uniforms'];
+  }
+}
---
+++ src/shaders/mandelbrot/2D/mandelbrotF64.frag
@@ -0,0 +1,78 @@
+precision highp float;
+
+/** iteration, dot, status, _ */
+layout(location = 0) out vec4 pc_result;
+/** z, iz, dz, diz */
+layout(location = 1) out vec4 pc_state;
+
+/** ITERATION_ON_FRAME, MAX_ITERATIONS */
+uniform vec2 u_const;
+/** 1/(zoom*height), width/2, height/2  */
+uniform vec3 u_view;
+/** ...offset */
+uniform vec2 u_offset;
+/** iteration, dot, status, NEVER */
+uniform sampler2D u_prev_result;
+/** z, iz, dz, diz */
+uniform sampler2D u_prev_state;
+
+#define NEVER 0.0
+#define INF_ESC -2.0
+#define PREC_ERR -3.0
+#define LIM_FRAME -4.0
+#define LIM_MAX -5.0
+
+void main() {
+    ivec2 pixelCoord = ivec2(gl_FragCoord.xy);
+    vec4 prevResult = texelFetch(u_prev_result, pixelCoord, 0);
+    vec4 prevState = texelFetch(u_prev_state, pixelCoord, 0);
+
+    int prevIterations = int(prevResult.x);
+
+    float status = prevResult.z;
+    if(prevIterations > 0) {
+        if(status == PREC_ERR || status == INF_ESC) {
+            pc_result = prevResult;
+            pc_state = prevState;
+            return;
+        }
+    }
+
+    vec2 z = prevIterations <= 0 ? vec2(0.0) : prevState.xy;
+    vec2 dz = prevIterations <= 0 ? vec2(0.0) : prevState.zw;
+
+    vec2 c = (gl_FragCoord.xy - u_view.yz) * u_view.x + u_offset.xy;
+
+    vec2 checkZ = z;
+    int power = 1;
+    int count = 0;
+
+    int iterationOnFrame = int(u_const.x);
+    for(int i = 0; i < iterationOnFrame; i++) {
+        vec2 v0 = z * z;
+        if((v0.x + v0.y) > 4.0) {
+            pc_result = vec4(float(prevIterations + i), (v0.x + v0.y), INF_ESC, NEVER);
+            pc_state = vec4(z, dz);
+            return;
+        }
+
+        z = vec2((v0.x - v0.y), 2.0 * z.x * z.y) + c;
+        if(z == checkZ) {
+            pc_result = vec4(float(prevIterations + i), (v0.x + v0.y), PREC_ERR, NEVER);
+            pc_state = vec4(z, dz);
+            return;
+        }
+
+        count++;
+        if(count >= power) {
+            checkZ = z;
+            power <<= 1;
+            count = 0;
+        }
+    }
+
+    float currentIteration = float(prevIterations + iterationOnFrame);
+    bool outOfMaxIterations = currentIteration >= u_const.y;
+    pc_result = vec4(currentIteration, dot(z, z), outOfMaxIterations ? LIM_MAX : LIM_FRAME, NEVER);
+    pc_state = vec4(z, dz);
+}
